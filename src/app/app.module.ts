8a44120d25247f6db9d0d79198eba0fad014d716
Liamarii <39436443+Liamarii@users.noreply.github.com>
2022-08-14 18:42:52 +0100

    Tasks can now be moved to another task list at an index and removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { TaskListComponent } from './task-list/task-list.component';
 import { PageHeaderComponent } from './page-header/page-header.component';
-import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
     TaskListComponent,
     PageHeaderComponent,
-    TaskComponent
   ],
   imports: [
     BrowserModule,
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,18 +1,175 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
-export class TaskListComponent{
-  public toDoTasks:Array<string> = [];
-  count = 1;
+export class TaskListComponent {
+  public toDoTasks: Array <string> = ["item 1", "item 2", "item 3", "item 4", "item 5"];
+  public doingTasks: Array <string> = ["item 1", "item 2", "item 3", "item 4"];
+  public doneTasks: Array <string> = ["item 1", "item 2"];
 
-  public AddTask(task: string) : void
+  currentSelection: string = "";
+  takenFromList: string = "";
+  hoveringOverTask: string = "";
+  hoveringOverTaskList: string = "";
+  emptyTaskListText: string = "No tasks";
+
+  public dragging(task: string, takenFromList: string) {
+      this.currentSelection = task;
+      this.takenFromList = takenFromList;
+  }
+
+  public dragover(event: Event, hoveringOverTask: string) {
+      event.preventDefault();
+      this.hoveringOverTask = hoveringOverTask;
+  }
+
+  public dropping(event: Event) {
+      event.preventDefault();
+      let targettedTaskListId = (event.target as HTMLElement).id;
+
+      if(this.takenFromList != targettedTaskListId){
+        this.moveTaskToDifferentTaskList(targettedTaskListId);
+      }
+      else{
+      switch (this.takenFromList) {
+        case 'todo':
+          this.reorderTaskInTaskList('todo');
+          break;
+        case 'doing':
+          this.reorderTaskInTaskList('doing');
+          break;
+        case 'done':
+          this.reorderTaskInTaskList('done');
+          break;
+      }
+    }
+  }
+
+  private reorderTaskInTaskList(targettedTaskList: string)
   {
-    this.toDoTasks = [...this.toDoTasks, task + this.count.toString()];
-    this.count++;
-  }  
-}
+    if (this.takenFromList == 'todo' && targettedTaskList == 'todo')
+    {     
+      let hoverIndex = this.toDoTasks.indexOf(this.hoveringOverTask);
+      this.removeDraggedTaskFromTaskList();
+      this.toDoTasks.splice(hoverIndex, 0, this.currentSelection);
+    }
+    if (this.takenFromList == 'doing' && targettedTaskList == 'doing')
+    {        
+      let hoverIndex = this.doingTasks.indexOf(this.hoveringOverTask);
+      this.removeDraggedTaskFromTaskList();
+      this.doingTasks.splice(hoverIndex, 0, this.currentSelection);
+    }
+    if (this.takenFromList == 'done' && targettedTaskList == 'done')
+    {
+      let hoverIndex = this.doneTasks.indexOf(this.hoveringOverTask);
+      this.removeDraggedTaskFromTaskList();
+      this.doneTasks.splice(hoverIndex, 0, this.currentSelection);
+    }
+  }
 
+  private moveTaskToDifferentTaskList(targettedTaskList : string)
+  {
+    if(targettedTaskList == 'todo' && this.toDoTasks.indexOf(this.currentSelection) == -1)
+    {
+      let hoverIndex = this.toDoTasks.indexOf(this.hoveringOverTask);
+      this.removeDraggedTaskFromTaskList();
+      this.toDoTasks.splice(hoverIndex, 0, this.currentSelection);
+      this.addPlaceholderTask();
+    }
+    if(targettedTaskList == 'doing' && this.doingTasks.indexOf(this.currentSelection) == -1)
+    {
+      let hoverIndex = this.doingTasks.indexOf(this.hoveringOverTask);
+      this.removeDraggedTaskFromTaskList();
+      this.doingTasks.splice(hoverIndex, 0, this.currentSelection);
+      this.addPlaceholderTask();
+    }
+    if(targettedTaskList == 'done' && this.doneTasks.indexOf(this.currentSelection) == -1)
+    {
+      let hoverIndex = this.doneTasks.indexOf(this.hoveringOverTask);
+      this.removeDraggedTaskFromTaskList();
+      this.doneTasks.splice(hoverIndex, 0, this.currentSelection);
+      this.addPlaceholderTask();
+    }
+  }
+
+  private removeDraggedTaskFromTaskList()
+  {    
+    switch (this.takenFromList) {
+      case 'todo':
+        this.toDoTasks = this.toDoTasks.filter(x => x !== this.currentSelection);
+        break;
+        case 'doing':
+        this.doingTasks = this.doingTasks.filter(x => x !==  this.currentSelection);
+        break;
+        case 'done':
+        this.doneTasks = this.doneTasks.filter(x => x !==  this.currentSelection);
+        break;
+    }
+  }
+
+  public removeSelectedTaskFromTaskList(task: string, taskList: Array<string>)
+  {    
+      if(taskList === this.toDoTasks)
+      {
+        this.toDoTasks = this.toDoTasks.filter(x => x !== task);
+        if(this.toDoTasks.length == 0)
+        {
+          this.toDoTasks.push(this.emptyTaskListText);
+        }
+      }
+      if(taskList === this.doingTasks)
+      {
+        this.doingTasks = this.doingTasks.filter(x => x !== task);
+        if(this.doingTasks.length == 0)
+        {
+          this.doingTasks.push(this.emptyTaskListText);
+        }
+      } 
+      if(taskList === this.doneTasks)
+      {
+        this.doneTasks = this.doneTasks.filter(x => x !== task);
+        if(this.doneTasks.length == 0)
+        {
+          this.doneTasks.push(this.emptyTaskListText);
+        }
+      } 
+  }
+
+  private addPlaceholderTask()
+  {
+    if(this.toDoTasks.length > 0)
+    {
+      this.toDoTasks = this.toDoTasks.filter(x => x !== this.emptyTaskListText);
+    }
+
+    if(this.toDoTasks.length == 0)
+    {
+      this.toDoTasks.push(this.emptyTaskListText);
+    }
+
+    if(this.doingTasks.length > 0)
+    {
+      this.doingTasks = this.doingTasks.filter(x => x !== this.emptyTaskListText);
+    }
+
+    if(this.doingTasks.length == 0)
+    {
+      this.doingTasks.push(this.emptyTaskListText);
+    }
+
+    if(this.doneTasks.length > 0)
+    {
+      this.doneTasks = this.doneTasks.filter(x => x !== this.emptyTaskListText);
+    }
+
+    if(this.doneTasks.length == 0)
+    {
+      this.doneTasks.push(this.emptyTaskListText);
+    }
+  }
+}
